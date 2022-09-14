aefa288b4cd13a1a786128cca6d8ae39d277e921
4dibbp90 <paul.dibble01@gmail.com>
2022-09-14 15:07:01 +0100

    API works
    able to pull data form open weather api.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WeatherTracker
 A weather tracking site
+
+##
--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 console.log ("hello world")
 
 const express = require('express');
-const index = express();
+const { appendFile } = require('fs');
 
-const http = require("http");
+const bodyParser = require("body-parser");
+
+const https = require('https');
+const app = express();
 
 const hostname = "127.0.0.1";
-const port = 8080;
+const port = 8000;
+ 
+// view engine ejs
+app.set('view engine', 'ejs')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-// Create HTTP server
-const server = http.createServer(function(req, res) {
+app.use(express.static("public"))
 
-   // Set the response HTTP header with HTTP status and Content type
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   //hosted
-   console.log ('hosted on http://localhost:8080/')
-   // Send the response body "Hello World"
-   res.end('Hello World\n');
-});
+      
 
 // Prints a log once the server starts listening
-server.listen(port, hostname, function() {
+app.listen(port, hostname, function() {
    console.log(`Server running at http://${hostname}:${port}/`);
 })
+// home
+app.get('/', (req, res) => {
+   
+   res.send('Hello World!')
+ })
+
+//pulling list of cities
+ app.get('/get', (req, res) => {
+//open weather api
+let url = "https://api.openweathermap.org/data/2.5/weather?q=Purnia&appid=f7acf97acded1337d827c9793c63907a&units=metric";
+https.get(url, (response) => {
+    if (response.statusCode === 200) {    
+        response.on("data", (data) => {
+        const results = JSON.parse(data);
+        console.log(results)
+    }) 
+    } else {
+        console.log("0")
+    }
+
+})
+      
+ });
